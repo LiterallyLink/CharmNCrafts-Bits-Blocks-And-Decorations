cee4dfeb9f6eb0fe6e77d8ac53d34b62e4598fac
Peoples <67402756+LiterallyLink@users.noreply.github.com>
2025-10-18 13:58:55 -0700

    Initial commit
    Initial commit

---
+++ src/client/java/com/charmed/charmncraft/bits/CharmncraftbitsClient.java
@@ -0,0 +1,10 @@
+package com.charmed.charmncraft.bits;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CharmncraftbitsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
