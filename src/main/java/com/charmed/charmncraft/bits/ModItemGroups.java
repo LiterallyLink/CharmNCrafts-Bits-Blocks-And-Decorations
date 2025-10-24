5151185258cb9e0ccbcf116db9bbaea2298c1d8d
Peoples <67402756+LiterallyLink@users.noreply.github.com>
2025-10-24 04:01:15 -0700

    CLEAN UP.

--- src/main/java/com/charmed/charmncraft/bits/ModItemGroups.java
+++ src/main/java/com/charmed/charmncraft/bits/ModItemGroups.java
@@ -10,15 +10,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroups {
-    public static final Identifier MORE_SLAB_VARIANTS_ID = Identifier.of(Charmncraftbits.MOD_ID, "more_slab_variants");
-    public static final RegistryKey<ItemGroup> MORE_SLAB_VARIANTS_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), MORE_SLAB_VARIANTS_ID);
-    
-    public static final ItemGroup MORE_SLAB_VARIANTS = FabricItemGroup.builder()
-        .displayName(Text.literal("More Slab Variants"))
-        .icon(() -> new ItemStack(Registries.ITEM.get(Identifier.of(Charmncraftbits.MOD_ID, "stone_slab"))))
-        .build();
 
     public static void initialize() {
-        Registry.register(Registries.ITEM_GROUP, MORE_SLAB_VARIANTS_ID, MORE_SLAB_VARIANTS);
     }
 }
